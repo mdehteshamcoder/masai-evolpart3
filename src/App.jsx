1b6d62c8714c887ec9431124cad6a0fd21512ca1
MD EHTESHAM <89984388+mdehteshamcoder@users.noreply.github.com>
2022-08-22 22:00:18 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import Slide from "./Components/Slide";
+import "./App.css";
+import React from "react";
+import { useEffect } from "react";
+
+// once you download the template; please run  "npm install" command and that will install all the packages required for this project by referencing package.json;
+
+// do not remove any of the data-cy attributes;
+
+// API to get the data : https://slides-app-220822.herokuapp.com/slides
+
+export default function App() {
+  return (
+    <div className="App">
+       
+      <h1 data-cy="header">Slides</h1>
+      <h3>Intro to React</h3>
+      <h3>ReactJS is a declarative, efficient, and flexible JavaScript library for building reusable UI components</h3>
+      <Slide />
+      <button  data-cy="prev" >Prev</button>
+      <button data-cy="next" >Next</button>
+    </div>
+  );
+}
